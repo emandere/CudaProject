78ff013292591c11091f5902b630f5db0ef5f8f4
emandere <emandere@gmail.com>
2017-12-26 01:32:31 -0500

    Update testSearch.cu

--- testSearch.cu
+++ testSearch.cu
@@ -4,7 +4,23 @@
 using namespace std;
 int main()
 {
-   int i=rand();
-   cout<<i<<endl;
+   int days = 12;
+   int seconds = 1000;
+   
+   float * prices = new float[N*N];
+   for(int i=0;i<N*N;i++)
+   {
+      prices[i]=rand()%100;  
+   }
+   
+   int sumprices;
+   for(int i=0;i<N*N;i=i+seconds)
+   {
+      sumprices=0;
+      for(int j=0;j<i+N;j++)
+         sumprices+=prices[j];
+      cout<<sumprices<<endl;
+   }
+   
    return 0;
 }
