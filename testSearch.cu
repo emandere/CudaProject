3d3fc2691fc61b26dd6bbe91b02ad3227f979293
emandere <emandere@gmail.com>
2017-12-26 01:53:54 -0500

    Update testSearch.cu

--- testSearch.cu
+++ testSearch.cu
@@ -2,13 +2,30 @@
 #include<iostream>
 #include <cstdlib>
 using namespace std;
+
+#define BLOCK_SIZE 32
+
+__global__ void gpuSum(int *prices,int *sumpricesout,int days,int seconds,int N)
+{
+    int currentday = blockIdx.x*blockDim.x + threadIdx.x;
+    if(currentday<days)
+    {
+       int start = currentday * seconds;
+       int end = (currentday * seconds)+seconds;
+       sumpricesout[currentday]=0;
+       for(int j=start;j<end;j++)
+         sumpricesout[currentday]+=prices[j]; 
+    }
+}
 int main()
 {
    int days = 12;
    int seconds = 1000;
    int N = days*seconds;
    
-   float * prices = new float[days*seconds];
+   int * prices = new int[days*seconds];
+   int  * sumpricesout = new int[days];
+   int sumprices;
    for(int i=0;i<N;i++)
    {
       prices[i]=rand()%100;  
@@ -18,7 +35,7 @@ int main()
    for(int i=0;i<N;i=i+seconds)
    {
       sumprices=0;
-      for(int j=0;j<i+seconds;j++)
+      for(int j=i;j<i+seconds;j++)
          sumprices+=prices[j];
       cout<<sumprices<<endl;
    }
