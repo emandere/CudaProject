2332898762580a0c8d1c7dea425f80bc3b406b0a
emandere <emandere@gmail.com>
2017-12-26 02:24:23 -0500

    Update testSearch.cu

--- testSearch.cu
+++ testSearch.cu
@@ -12,7 +12,7 @@ __global__ void gpuSum(int *prices,int *sumpricesout,int days,int seconds,int N)
     {
        int start = currentday * seconds;
        int end = (currentday * seconds)+seconds;
-       sumpricesout[currentday]=days;
+       sumpricesout[currentday]=start;
        for(int j=start;j<end;j++)
          sumpricesout[currentday]+=prices[j]; 
     }
